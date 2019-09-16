ce99e6e56c16ace64028c5093f3c62690dad8a5a
Wasin Thonkaew <haxpor@gmail.com>
2019-09-16 19:11:27 +0800

    Add Quicksort

---
+++ Algorithm/Quicksort.cpp
@@ -0,0 +1,54 @@
+/**
+ * Implementation of quicksort algorithm.
+ *
+ * Quicksort is inplace sorting algorithm which is based on divide-and-conquer approach.
+ * Interesting note is that quicksort is used as part of std::sort (which applies 3 sorting algorithms
+ * together which are quicksort, heapsort and insertion sort) 's sorting algorithm namely introsort.
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define PRINTARRAY(v) for(const int& e: v) cout << e << " "; cout << endl;
+
+static int partition(vector<int>& v, int low, int high)
+{
+    // select last element as pivot
+    int pivot = v[high];
+    int i = low;
+
+    for (int j=low; j<high; ++j)
+    {
+        if (v[j] < pivot)
+        {
+            swap(v[i], v[j]);
+            ++i;
+        }
+    }
+    swap(v[i], v[high]);
+    return i;
+}
+
+static void quicksort(vector<int>& v, int l, int r)
+{
+    if (l < r)
+    {
+        int q = partition(v, l, r);
+        quicksort(v, l, q-1);
+        quicksort(v, q+1, r);
+    }
+}
+
+int main()
+{
+    vector<int> v ({9, 7, 5, 11, 12, 2, 14, 3, 10, 6});
+    PRINTARRAY(v)
+
+    quicksort(v, 0, v.size()-1);
+    PRINTARRAY(v)
+    
+    return 0;
+}
+
