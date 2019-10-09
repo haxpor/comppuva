a19414ecde42ddb00077af30b4c361adab1f839d
Wasin Thonkaew <haxpor@gmail.com>
2019-10-09 08:39:50 +0800

    Add solution to Tower of Hanoi

---
+++ TowerOfHanoi.cpp
@@ -0,0 +1,46 @@
+/**
+ * Solution on Tower of Hanoi problem.
+ *
+ * Note:
+ *  - It's easier to think in terms of moving N-1 disks from starting rod to destination rod.
+ *    This makes it easy to come up with algorithm to implement in code.
+ *    Lastly when there is just 1 disk left, move it to the destination rod.
+ *    Especially see this explanation https://www.geeksforgeeks.org/c-program-for-tower-of-hanoi/.
+ *
+ *  - The reason why print function call needs to be between two recursive function calls
+ *    is because of print function call is also as part of the procedure but it's just printing something
+ *    out. See https://www.cs.cmu.edu/~cburch/survey/recurse/hanoiimpl.html.
+ *
+ */
+#include <iostream>
+
+/**
+ * Move n disks from rod 'from' to rod 'to' with auxilary rod 'aux'.
+ * Rods are represented as characters.
+ */
+static void towerHanoiMove(int n, char from, char to, char aux)
+{
+    // if it's just left with 1 disk, then we can directly move it to destinatino rod
+    if (n == 1)
+    {
+        std::cout << "move disk 1 from " << from << " to " << to << std::endl;
+    }
+    // otherwise recursively move disks
+    else
+    {
+        // the whole procedure can be described as a big picture of 3 steps as follows
+        // move n-1 disks from 'A' to 'B'
+        towerHanoiMove(n-1, from, aux, to);
+        // now we're left with the last disk in 'A', then move it to 'C'
+        std::cout << "move disk " << n << " from " << from << " to " << to << std::endl;
+        // move n-1 disks from 'B' to 'C'
+        towerHanoiMove(n-1, aux, to, from);
+    }
+}
+
+int main()
+{
+    // the problem set has 4 disks with objective to move from rod 'A' to 'C' with auxilary at rod 'B'.
+    towerHanoiMove(3, 'A', 'C', 'B');
+    return 0;
+}
