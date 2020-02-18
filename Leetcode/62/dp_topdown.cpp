98d1ac95d90cb08c52585d5f153dcebe9e183042
Wasin Thonkaew <haxpor@gmail.com>
2020-02-18 22:08:33 +0700

    Add DP topdown approach to Leetcode 62

--- Leetcode/62/dp_topdown.cpp
+++ Leetcode/62/dp_topdown.cpp
@@ -19,11 +19,43 @@
 #include <fstream>
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 
 class Solution {
 public:
     int uniquePaths(int m, int n) {
+        if (m == 1 && n == 1)
+            return 1;
+
+        // max value of m, or n is at most 100
+        std::vector<std::vector<int>> dp (n, std::vector<int>(m));
+        for (int i=0; i<n; ++i)
+            std::memset(&dp[i][0], -1, sizeof(int) * dp[i].size());
+        dp[0][0] = 0;
+
+        // base cases
+        for (int col=1; col<m; ++col)
+            dp[0][col] = 1;
+        for (int row=1; row<n; ++row)
+            dp[row][0] = 1;
+
+        return solve(m-1, n-1, dp);
+    }
+private:
+    // solve for target m,n of grid
+    // m and n are index-based
+    int solve(int m, int n, std::vector<std::vector<int>>& dp) {
+        if (dp[n][m] != -1)
+            return dp[n][m];
         
+        // if able to find nearby positions, then return compute the answer for this position in the grid
+        if (dp[n-1][m] != -1 && dp[n][m-1] != -1) {
+            dp[n][m] = dp[n-1][m] + dp[n][m-1];
+            return dp[n][m];
+        }
+        else {
+            return solve(m, n-1, dp) + solve(m-1, n, dp);
+        }
     }
 };
 
