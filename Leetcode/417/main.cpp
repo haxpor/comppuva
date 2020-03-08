cbc71d7e262f1a74c773fac8399fba415f0140c3
Wasin Thonkaew <haxpor@gmail.com>
2020-03-08 19:57:55 +0700

    Add Leetcode 417 - Pacific Atlantic Water Flow

---
+++ Leetcode/417/main.cpp
@@ -0,0 +1,120 @@
+/**
+ * Problem 417 - Pacific Atlantic Water Flow 
+ *
+ * Format of testcase.txt is as follows
+ *
+ * Number of testcases
+ * TC#1 Array of Array of rows representing the matrix
+ * TC#1 The expect output
+ * TC#2 ... and so on
+ *
+ * Time complexity: <to-be-filled>
+ * Space complexity: <to-be-filled>
+ */
+#include <vector>
+#include <cassert>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+#include "ArrayInputParser.h"
+
+class Solution {
+public:
+    std::vector<std::vector<int>> pacificAtlantic(std::vector<std::vector<int>>& matrix) {
+        if (matrix.size() == 0)
+            return {};
+
+        M = matrix.size();
+        N = matrix[0].size();
+        
+        std::vector<std::vector<bool>> pacific (M, std::vector<bool>(N, false));
+        std::vector<std::vector<bool>> atlantic (M, std::vector<bool>(N, false));
+
+        for (int col=0; col<N; ++col) {
+            dfs(matrix, pacific, 0, col, -99);
+            dfs(matrix, atlantic, M-1, col, -99);
+        }
+
+        for (int row=0; row<M; ++row) {
+            dfs(matrix, pacific, row, 0, -99);
+            dfs(matrix, atlantic, row, N-1, -99);
+        }
+
+        std::vector<std::vector<int>> result;
+        for (int row=0; row<M; ++row) {
+            for (int col=0; col<N; ++col) {
+                if (pacific[row][col] && atlantic[row][col])
+                    result.push_back({row, col});
+            }
+        }
+
+        return result;
+    }
+private:
+    int M;
+    int N;
+
+    // make a decision based on the specified `row` and `col` within the grid.
+    void dfs(const std::vector<std::vector<int>>& matrix, std::vector<std::vector<bool>>& ocean, int row, int col, int prevVal) {
+        if (row < 0 || col < 0 || row >= M || col >= N || ocean[row][col] || matrix[row][col] < prevVal)
+            return;
+
+        ocean[row][col] = true;
+        int baseVal = matrix[row][col];
+
+        dfs(matrix, ocean, row+1, col, baseVal);    // up
+        dfs(matrix, ocean, row, col+1, baseVal);    // right
+        dfs(matrix, ocean, row-1, col, baseVal);    // down
+        dfs(matrix, ocean, row, col-1, baseVal);    // left
+    }
+};
+
+int main()
+{
+    std::ifstream infile("testcases.txt");
+    std::string line;
+
+    int N;
+    infile >> N;    // read number of testcases
+    infile.ignore();
+
+    Solution sol;
+    ArrayInputParser aparser;
+
+    for (int i=0; i<N; ++i)
+    {
+        // read in matrix
+        std::getline(infile, line);
+        std::vector<std::vector<int>> matrix = aparser.parse2(line);
+
+        // read expect result
+        std::getline(infile, line);
+        std::vector<std::vector<int>> expect = aparser.parse2(line);
+
+        // compute
+        std::vector<std::vector<int>> out = sol.pacificAtlantic(matrix);
+
+        // order is not mandatory thus sort them both before comparing
+        auto sortCompare = [](const std::vector<int>& a, const std::vector<int>& b) {
+            return a[0] < b[0] || (a[0] == b[0] && a[1] < b[1]);
+        };
+        std::sort(out.begin(), out.end(), sortCompare);
+        std::sort(expect.begin(), expect.end(), sortCompare);
+
+        if (out == expect)
+        {
+            std::printf("[Success #%d]\n", i);
+        }
+        else
+        {
+            std::printf("[Failed #%d] Output mismatch\n", i);
+        }
+    }
+
+    infile.close();
+
+    return 0;
+}
