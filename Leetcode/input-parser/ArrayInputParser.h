9bfb9b5c5634648efc9b6db3c379970a6c8c20d3
Wasin Thonkaew <haxpor@gmail.com>
2020-03-08 23:19:50 +0700

    Update ArrayInputParser to support element of vector as std::vector<char> and moved it into template

--- Leetcode/input-parser/ArrayInputParser.h
+++ Leetcode/input-parser/ArrayInputParser.h
@@ -2,6 +2,9 @@
 
 #include <vector>
 #include <string>
+#include <stack>
+#include <stdexcept>
+#include <iostream>
 
 /**
  * User still need to know which format to read and choose appropriately from either parse() or parse2()
@@ -9,6 +12,166 @@
  */
 class ArrayInputParser {
 public:
-    std::vector<int> parse(const std::string& str) const;
-    std::vector<std::vector<int>> parse2(const std::string& str) const;
+    template <typename T>
+    std::vector<T> parse(const std::string& str) const {
+        throw std::logic_error("Not implemented");
+    }
 };
+
+template <>
+std::vector<int> ArrayInputParser::parse<int>(const std::string& str) const {
+    if (str.size() == 0)
+        return {};
+
+    std::stack<char> st;
+    std::vector<int> resVector;
+    std::string tmpIntStr = "";
+
+    for (const char& c : str) {
+        if (c == '[')
+            st.push(c);
+        else if (c  >= '0' && c <= '9')
+            tmpIntStr += c;
+        else if (c == ',') {
+            resVector.push_back(std::stoi(tmpIntStr));
+            tmpIntStr = ""; 
+        }
+        else if (c == ']') {
+            if (tmpIntStr != "") {
+                resVector.push_back(std::stoi(tmpIntStr));
+                tmpIntStr = "";
+            }
+            st.pop();
+        }
+    }
+
+    // validate correctness
+    if (st.size() != 0)
+        throw std::runtime_error("stack size should be 0. Input string is '" + str + "'");
+
+    return resVector;
+}
+
+template <>
+std::vector<char> ArrayInputParser::parse<char>(const std::string& str) const {    
+     if (str.size() == 0)
+        return {};
+
+    std::stack<char> st;
+    std::vector<char> resVector;
+    bool elemAddState = false;
+
+    for (const char& c : str) {
+        if (c == '[')
+            st.push(c);
+        else if (c == ']')
+            st.pop();
+        else if (!elemAddState && c == '"') {
+            st.push(c);
+            elemAddState = true;
+        }
+        else if (elemAddState && c == '"') {
+            st.pop();
+            elemAddState = false;
+        }
+        else if (elemAddState && c != '"')
+            resVector.push_back(c);
+    }
+
+    // validate correctness
+    if (st.size() != 0)
+        throw std::runtime_error("stack size should be 0. Input string is '" + str + "'");
+
+    return resVector;
+}
+
+template <>
+std::vector<std::vector<int>> ArrayInputParser::parse<std::vector<int>>(const std::string& str) const {
+    if (str.size() == 0)
+    return {};      
+
+    std::stack<char> st;
+    std::vector<std::vector<int>> resVector;
+    std::vector<int> tmpVector;
+    std::string tmpIntStr = "";
+
+    int leftParenCount = 0;
+
+    for (const char& c : str) {
+        if (c == '[') {
+            ++leftParenCount;
+            if (leftParenCount >= 2)
+                tmpVector.clear();
+            st.push(c);
+        }
+        else if (c  >= '0' && c <= '9' && leftParenCount >= 2) {
+            tmpIntStr += c;
+        }
+        else if (c == ',' && leftParenCount >= 2) {
+            tmpVector.push_back(std::stoi(tmpIntStr));
+            tmpIntStr = ""; 
+        }
+        else if (c == ']') {
+            if (leftParenCount >= 2) {
+                if (tmpIntStr != "") {
+                    tmpVector.push_back(std::stoi(tmpIntStr));
+                }
+                resVector.emplace_back(std::move(tmpVector));
+                tmpIntStr = "";
+            } 
+            --leftParenCount;
+            st.pop();
+        }
+    }
+
+    // validate correctness
+    if (st.size() != 0)
+        throw std::runtime_error("stack size should be 0. Input string is '" + str + "'");
+
+    return resVector; 
+}
+
+template <>
+std::vector<std::vector<char>> ArrayInputParser::parse<std::vector<char>>(const std::string& str) const {
+    if (str.size() == 0)
+        return {};      
+
+    std::stack<char> st;
+    std::vector<std::vector<char>> resVector;
+    std::vector<char> tmpVector;
+    bool elemAddState = false;
+    int leftParenCount = 0;
+
+    for (const char& c : str) {
+        if (c == '[') {
+            ++leftParenCount;
+            if (leftParenCount >= 2)
+                tmpVector.clear();
+            st.push(c);
+        }
+        else if (!elemAddState && c == '"') {
+            st.push(c);
+            elemAddState = true;
+        }
+        else if (elemAddState && c == '"') {
+            st.pop();
+            elemAddState = false;
+        }
+        else if (elemAddState && c != '"') {
+            tmpVector.push_back(c);
+        }
+        else if (c == ']') {
+            if (leftParenCount >= 2) {
+                resVector.emplace_back(std::move(tmpVector));
+            } 
+            --leftParenCount;
+            st.pop();
+        }
+    }
+
+    // validate correctness
+    if (st.size() != 0)
+        throw std::runtime_error("stack size should be 0. Input string is '" + str + "'");
+
+    return resVector; 
+}
--- Leetcode/input-parser/main.cpp
+++ Leetcode/input-parser/main.cpp
@@ -21,30 +21,36 @@ int main()
     ArrayInputParser parser;
     // 1d vector test
     {
-        std::vector<int> out = parser.parse("[]");
+        std::vector<int> out = parser.parse<int>("[]");
         assert(out.size() == 0 && "size of array should be 0");
     }
     {
-        std::vector<int> out = parser.parse("[1]");
+        std::vector<int> out = parser.parse<int>("[1]");
         assert(out.size() == 1 && "size of array should be 1");
         assert(out[0] == 1 && "first element should be 1");
     }
     {
-        std::vector<int> out = parser.parse("[1,2,3,4]");
+        std::vector<int> out = parser.parse<int>("[1,2,3,4]");
         assert(out.size() == 4 && "size of array should be 4");
         assert(out[0] == 1 && "first element should be 1");
         assert(out[1] == 2 && "first element should be 2");
         assert(out[2] == 3 && "first element should be 3");
         assert(out[3] == 4 && "first element should be 4");
     }
+    {
+        std::vector<char> out = parser.parse<char>("[\"1\",\"2\"]");
+        assert(out.size() == 2 && "size of array should be 2");
+        assert(out[0] == '1' && "first element should be '1'");
+        assert(out[1] == '2' && "first element should be '2'");
+    }
 
     // 2d vector tests
     {
-        std::vector<std::vector<int>> out = parser.parse2("[[]]");
+        std::vector<std::vector<int>> out = parser.parse<std::vector<int>>("[[]]");
         assert(out.size() == 1 && "size of 2d-array should be 1");
     }
     {
-        std::vector<std::vector<int>> out = parser.parse2("[[1],[2,3]]");
+        std::vector<std::vector<int>> out = parser.parse<std::vector<int>>("[[1],[2,3]]");
         assert(out.size() == 2 && "out.size() should be 2");
         assert(out[0].size() == 1 && "out[0].size() should be 1");
         assert(out[1].size() == 2 && "out[1].size() should be 2");
@@ -53,7 +59,7 @@ int main()
         assert(out[1][1] == 3 && "out[1][0] should be 3");
     }
     {
-        std::vector<std::vector<int>> out = parser.parse2("[[1,2],[3,4]]");
+        std::vector<std::vector<int>> out = parser.parse<std::vector<int>>("[[1,2],[3,4]]");
         assert(out.size() == 2 && "size of 2d-array should be 2");
         assert(out[0].size() == 2 && "size of out[0] should be 2");
         assert(out[1].size() == 2 && "size of out[1] should be 2");
@@ -61,6 +67,18 @@ int main()
         assert(out[0][1] == 2 && "out[0][1] should be 2");
         assert(out[1][0] == 3 && "out[0][0] should be 3");
         assert(out[1][1] == 4 && "out[0][1] should be 4");
+    }
+    {
+        std::vector<std::vector<char>> out = parser.parse<std::vector<char>>("[[\"1\",\"1\",\"0\"],[\"1\",\"1\",\"0\"]]");
+        assert(out.size() == 2 && "size of 2d-array should be 2");
+        assert(out[0].size() == 3 && "size of out[0] should be 3");
+        assert(out[1].size() == 3 && "size of out[1] should be 3");
+        assert(out[0][0] == '1');
+        assert(out[0][1] == '1');
+        assert(out[0][2] == '0');
+        assert(out[1][0] == '1');
+        assert(out[1][1] == '1');
+        assert(out[1][2] == '0');
     }
     return 0;
 }
